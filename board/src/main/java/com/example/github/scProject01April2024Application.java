c67f1283db74108fde1055f6cafb0bf5e7369d88
soheeparklee <soheeparklee@gmail.com>
2024-04-02 15:06:50 +0100

    first commit

--- board/src/main/java/com/example/github/scProject01April2024Application.java
+++ board/src/main/java/com/example/github/scProject01April2024Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoardApplication {
+public class scProject01April2024Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BoardApplication.class, args);
+		SpringApplication.run(scProject01April2024Application.class, args);
 	}
 
 }
--- board/src/test/java/com/example/github/scProject01April2024ApplicationTests.java
+++ board/src/test/java/com/example/github/scProject01April2024ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BoardApplicationTests {
+class scProject01April2024ApplicationTests {
 
 	@Test
 	void contextLoads() {
